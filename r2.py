45cfd57eb7c79c88002baea6480cdf138208d20e
Andy Shih <Tuonghao0318@icloud.com>
2021-01-22 13:24:53 +1100

    chat part 2

---
+++ r2.py
@@ -0,0 +1,57 @@
+# 讀取檔案
+def read_file(filename):
+	lines = []
+	with open(filename, 'r', encoding='utf-8-sig') as f:
+		for line in f:
+			lines.append(line.strip())
+	return lines
+
+# 轉換檔案
+def convert(lines):
+	person = None
+	andy_word_count = 0
+	tom_word_count = 0
+	for line in lines:
+		s = line.split(' ')
+		time = s[0]
+		name = s[1]
+		if name == 'Andy':
+			for m in s[2:]:
+				andy_word_count += len(m)
+		elif name == 'Tom':
+			for m in s[2:]:
+				tom_word_count += len(m)
+	print('andy說了', andy_word_count)
+	print('tom說了', tom_word_count)	
+		# print(s)
+
+
+# 寫入檔案
+def write_file(filename, lines):
+	with open(filename, 'w') as f:
+		for line in lines:
+			f.write(line + '\n')
+
+# 主要功能
+def main():
+	lines = read_file('tom.txt')
+	lines = convert(lines)
+	# write_file('output.txt', lines)
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
